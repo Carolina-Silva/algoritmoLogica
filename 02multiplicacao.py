525b38a27f5ccd5ad2a3e927c18804242d85879a
Carolina-Silva <carol_paiva@outlook.com>
2022-02-23 21:36:05 -0300

    exercicios 1 a 3 python

---
+++ 02multiplicacao.py
@@ -0,0 +1,4 @@
+A = int(input())
+B = int(input())
+multiplicacao = A * B
+print ("MULTIPLICAÇÃO = %i" % multiplicacao)
---
+++ 03retangulo.py
@@ -0,0 +1,4 @@
+largura = int(input("Por favor informe a largura do retângulo"))
+altura = int(input("Por favor informe a altura do retângulo"))
+area = largura + altura
+print ("SOMA = %i" % area)
