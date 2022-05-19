a5c2175052cf107f473c77dd50ac6fe94cdcbfae
Carolina Silva <65463404+Carolina-Silva@users.noreply.github.com>
2022-05-19 11:28:03 -0300

    dicionario

---
+++ dicionario.py
@@ -0,0 +1,16 @@
+placa = input("placa")
+ult_N = placa[len(placa)-1:len(placa)]
+
+rodizio = {
+    "1":"Seg"
+    "2":"Seg"
+    "3":"Ter"
+    "4":"Ter"
+    "5":"Qua"
+    "6":"Qua"
+    "7":"Qui"
+    "8":"Qui"
+    "9":"Sex"
+    "0":"Sex"
+}
+print(rodizio[ult_N])
